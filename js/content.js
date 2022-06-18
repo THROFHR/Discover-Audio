ef8dd4ceeba1b7525e5ffb114376c075bbba0f00
ranxfan <ranxfan@gmail.com>
2022-06-18 23:19:29 +0800

    feat:优化流程

--- js/content.js
+++ js/content.js
@@ -19,13 +19,42 @@ var initContent = function (html) {
     showActions()
     /**监听下载按钮 */
     $('body').on('click', '#inject-action-btn-down', function () {
+        
+        $('.inject-dom-popup-card').show()
+        // chrome.runtime.sendMessage({
+        //     url: data.url,
+        //     title: title || document.title,
+        //     href: location.href
+        // }, (response) => {
+        //     // response 是 background 或者 popupjs 响应后的回复
+        // });
+    });
+
+    $('body').on('click', '.inject-dom-popup-card-main-button-comfirm', function () {
+        var title = $('#inject-dom-popup-card-main-input-title').val();
+        if (!title) {
+            alert("输入保存名称")
+            return;
+        }
+        var statusDom = document.querySelector('.inject-dom-popup-card-main-status')
+        statusDom.style.zIndex = 1;
+        statusDom.style.opacity = 1;
+        setTimeout(()=>{
+            $('.inject-dom-popup-card').hide()
+            statusDom.style.zIndex = -1;
+            statusDom.style.opacity = 0;
+        },1000);
         chrome.runtime.sendMessage({
             url: data.url,
-            title: document.title,
+            title: title,
             href: location.href
         }, (response) => {
             // response 是 background 或者 popupjs 响应后的回复
         });
+    });
+    $('body').on('click', '.inject-dom-popup-card-main-button-cancel', function () {
+        $('.inject-dom-popup-card-main-input-title').val("");
+        $('.inject-dom-popup-card').hide()
     });
     console.log('inject finish');
 }
